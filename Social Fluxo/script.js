fd6c34cd28d9e5ee57892cbed3e4cbfb9807e714
Carolina Santiago <ctt.carolinasantiago@gmail.com>
2024-06-01 21:27:46 -0300

    add comentarios no js

--- Social Fluxo/script.js
+++ Social Fluxo/script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Seleciona o botão de enviar
-  const enviar = document.querySelector(".btn-login");
+  const enviar = document.querySelector(".btn-login"); // Seleciona o botão de enviar
 
   enviar.addEventListener("click", function (event) {
     event.preventDefault(); // Impede o envio do formulário
 
-    const emailInput = document.getElementById("email");
-    const emailValue = emailInput.value;
+    const emailInput = document.getElementById("email"); // Seleciona o input de email
+    const emailValue = emailInput.value; // Guarda o valor do input de email
 
-    const passwordInput = document.getElementById("password");
-    const passwordValue = passwordInput.value;
+    const passwordInput = document.getElementById("password"); // Seleciona o input de senha
+    const passwordValue = passwordInput.value; // Guarda o valor do input de senha
 
-    console.log("Email:", emailValue);
-    console.log("Senha:", passwordValue);
+    // console.log("Email:", emailValue);
+    // console.log("Senha:", passwordValue);
 
     alert(
       "Login realizado com sucesso!\n\n" +
